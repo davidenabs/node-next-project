8cecff136f9b44a6b9d64feb4dd4c6863dbcbffc
David Enabs <davidenabs@gmail.com>
2024-10-17 09:46:51 +0100

    "Updated .env file with new MongoDB URI, added mongodb package, and refactored MongoDB connection code in app.js"

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { config } from 'dotenv';
 import routes from './app/routes.js';
 import errorHandler from './app/middlewares/error.js';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 config();
 const app = express();
@@ -12,9 +13,32 @@ app.use(cors());
 app.use(json());
 app.use(errorHandler)
 
-connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+// connect(process.env.MONGO_URI)
+//     .then(() => console.log("MongoDB connected"))
+//     .catch(err => console.log(err));
+
+const uri = process.env.MONGO_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 //Allow all requests from all domains & localhost
 app.all('/*', function (req, res, next) {
